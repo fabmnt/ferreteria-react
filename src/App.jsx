e00d0cbd92e20373c10da3c2dc4f28fa1bc6e6af
Fabian Montoya <fabian.montoya119@est.unanleon.edu.ni>
2024-09-05 23:01:41 -0600

    Adding title

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import './App.css'
 
 const App = () => {
   return (
-    <div className='content'>
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div className='w-full h-screen flex justify-center items-center bg-black text-white'>
+      <h1>Ferreteria</h1>
     </div>
   )
 }
