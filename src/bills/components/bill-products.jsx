f777000ba5be1a544c5bd13e86378a6280cd239e
Fabian Montoya <fabian.montoya119@est.unanleon.edu.ni>
2024-10-04 15:12:55 -0600

    Refactor createCustomer function to select inserted data

--- src/bills/components/bill-products.jsx
+++ src/bills/components/bill-products.jsx
@@ -2,6 +2,7 @@ import { IoIosAdd } from 'react-icons/io'
 import { Input } from '../../components/input'
 import { useState } from 'react'
 import { AiOutlineDelete } from 'react-icons/ai'
+import { Button } from 'flowbite-react'
 
 export function BillProducts({
   openAddProductModal,
@@ -15,7 +16,7 @@ export function BillProducts({
   const handleUpdateQuantity = (e, productId) => {
     e.preventDefault()
     const inputQuantity = e.target.value
-    const newProductsQuantity = { ...quantities  }
+    const newProductsQuantity = { ...quantities }
     const product = products.find((product) => product.id === productId)
 
     if (product == null) {
@@ -36,7 +37,7 @@ export function BillProducts({
     updateProductsQuantity(newProductsQuantity)
   }
 
-  const removeProduct = (productId) => {  
+  const removeProduct = (productId) => {
     const updateProducts = products.filter((product) => product.id !== productId)
     setProducts(updateProducts)
 
@@ -48,86 +49,84 @@ export function BillProducts({
 
   return (
     <>
-      <div className='mb-4 flex items-center justify-between'>
+      <div className='mb-4 flex justify-between'>
         <h4 className='font-semibold'>Productos</h4>
-        <button
+        <Button
+          outline
+          size='xs'
+          color='light'
           onClick={() => openAddProductModal()}
-          className='flex items-center justify-center gap-2 text-sm'
         >
-          <IoIosAdd className='size-6' />
-          <span>Agregar producto</span>
-        </button>
+          <div className='flex items-center justify-center gap-2 text-sm'>
+            <IoIosAdd className='size-6' />
+            <span>Agregar producto</span>
+          </div>
+        </Button>
       </div>
       {products.length === 0 && (
         <div className='flex h-60 w-full items-center justify-center rounded-xl border border-dashed border-gray-200 p-6'>
-          <span className='text-2xl font-semibold text-gray-600 opacity-60'>
+          <span className='text-lg font-semibold text-gray-600 opacity-60'>
             Añade productos a la factura
           </span>
         </div>
       )}
 
       {products.length > 0 && (
-        <table className='sticky top-0 z-10 w-full table-auto bg-white text-left text-sm'>
-          <thead className='sticky top-0 z-10 border-b bg-white'>
-            <tr>
-              <th>#</th>
-              <th>Producto</th>
-              <th>Marca</th>
-              <th>Precio</th>
-              <th>Cantidad</th>
-              <th className='w-[10ch]'>
-                Total
-              </th>
-              <th className='w-[5ch] '>
-                Acciones
-              </th>
-            </tr>
-          </thead>
-          <tbody className=''>
-            {products.map((product) => (
-              <tr
-                className='[&>td]:h-10 [&>td]:align-middle'
-                key={product.id}
-              >
-                <td>{product.id}</td>
-                <td>{product.name}</td>
-                <td>{product.brand}</td>
-                <td>C$ {product.price}</td>
-                <td>
-                  <Input
-                    type='number'
-                    min={1}
-                    max={product.stock}
-                    className='w-[6ch] px-2 py-1'
-                    value={quantities[product.id] ?? 1}
-                    onInput={(e) => handleUpdateQuantity(e, product.id)}
-                  />
-                </td>
-                <td>
-                  C$ {product.price * (quantities[product.id] ?? 1)}
-                </td>
-                <td className='flex justify-center'>
-                  <button
-                    onClick={() => removeProduct(product.id)}
-                  >
-                    <AiOutlineDelete className='h-6 w-5 text-red-700' />
-                  </button>
-                </td>
+        <div className='mt-2 h-[200px] overflow-y-auto scroll-smooth'>
+          <table className='sticky top-0 z-10 w-full table-auto bg-white text-left text-sm'>
+            <thead className='sticky top-0 z-10 border-b bg-white'>
+              <tr className='text-xs [&>th]:py-2 [&>th]:font-normal [&>th]:text-neutral-600'>
+                <th>#</th>
+                <th>Producto</th>
+                <th>Marca</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th className='w-[10ch]'>Total</th>
+                <th className='w-[5ch]' />
               </tr>
-            ))}
-
-            {products.length === 0 && (
-              <tr>
-                <td
-                  colSpan='6'
-                  className='text-center'
+            </thead>
+            <tbody className=''>
+              {products.map((product) => (
+                <tr
+                  className='[&>td]:h-10 [&>td]:align-middle'
+                  key={product.id}
                 >
-                  <span className='text-gray-500'>No hay productos disponibles.</span>
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+                  <td>{product.id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.brand}</td>
+                  <td>C$ {product.price}</td>
+                  <td>
+                    <Input
+                      type='number'
+                      min={1}
+                      max={product.stock}
+                      className='w-[6ch] p-1 text-center'
+                      value={quantities[product.id] ?? 1}
+                      onInput={(e) => handleUpdateQuantity(e, product.id)}
+                    />
+                  </td>
+                  <td>C$ {product.price * (quantities[product.id] ?? 1)}</td>
+                  <td className='flex justify-center'>
+                    <button onClick={() => removeProduct(product.id)}>
+                      <AiOutlineDelete className='h-6 w-5 text-red-700' />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+
+              {products.length === 0 && (
+                <tr>
+                  <td
+                    colSpan='6'
+                    className='text-center'
+                  >
+                    <span className='text-gray-500'>No hay productos disponibles.</span>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       )}
     </>
   )
