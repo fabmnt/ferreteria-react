18ec0b1e62307d98acf29acd578928e04a45e535
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-11-03 21:55:30 -0600

    Refactor Dashboard layout to include MostFrequentClientsTable and MostSelledProductsChart components

--- src/dashboard/page.jsx
+++ src/dashboard/page.jsx
@@ -1,11 +1,8 @@
-import { Button, Datepicker } from 'flowbite-react'
+import { MostFrequentClientsTable } from './components/most-frequent-clients-table'
+import { MostSelledProductsChart } from './components/most-selled-products-chart'
 import { SellsChart } from './components/sells-chart'
 
 export function Dashboard() {
-  const handleChangeDate = (date) => {
-    console.log(date)
-  }
-
   return (
     <div>
       <div className='flex justify-between'>
@@ -13,23 +10,31 @@ export function Dashboard() {
           <h2 className='text-2xl font-semibold'>Dashboard</h2>
           <p className='text-sm text-neutral-600'>Información principal de la ferretería.</p>
         </div>
-        <div>
-          <Button
-            color='light'
-            size='sm'
-          >
-            <div className='flex items-center gap-2'>
-              <Datepicker onChange={handleChangeDate} />
-            </div>
-          </Button>
-        </div>
       </div>
       <section className='mt-4 grid grid-cols-2 gap-4'>
-        <div className='rounded border bg-white p-4'>
-          <SellsChart />
+        <div className='h-[350px] rounded border bg-white p-4'>
+          <div className='mb-2 flex items-center gap-1'>
+            <h4 className='font-medium'>Total de ventas</h4>
+            <p className='text-sm text-neutral-600'>Últimos 15 días</p>
+          </div>
+          <div className='h-full w-full p-4'>
+            <SellsChart />
+          </div>
         </div>
-        <div className='h-[220px] rounded border bg-white p-4' />
-        <div className='col-span-2 h-[420px] rounded border bg-white p-4' />
+        <div className='h-[350px] rounded border bg-white p-4'>
+          <div className='mb-2'>
+            <h4 className='font-medium'>Productos más vendidos </h4>
+          </div>
+          <div className='h-full w-full p-4'>
+            <MostSelledProductsChart />
+          </div>
+        </div>
+        <div className='col-span-2 rounded border bg-white p-4'>
+          <div className='mb-2'>
+            <h4 className='font-medium'>Clientes más recurrentes</h4>
+          </div>
+          <MostFrequentClientsTable />
+        </div>
       </section>
     </div>
   )
